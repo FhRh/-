2f6038095f26ce4bd8eab4266b263b11c37b6175
Due_it <48638700+FhRh@users.noreply.github.com>
2023-05-24 21:23:28 +0900

    Add files via upload
    AOP 적용 예제

---
+++ 7-1 AOP/src/main/java/hello/hellospring/SpringConfig.java
@@ -0,0 +1,36 @@
+package hello.hellospring;
+
+import hello.hellospring.aop.TimeTraceAop;
+import hello.hellospring.repository.JdbcMemberRepository;
+import hello.hellospring.repository.JpaMemberRepository;
+import hello.hellospring.repository.MemberRepository;
+import hello.hellospring.repository.MemoryMemberRepository;
+import hello.hellospring.service.MemberService;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityManager;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class SpringConfig {
+
+    private final MemberRepository memberRepository;
+
+    @Autowired
+    public SpringConfig(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
+    @Bean
+    public MemberService memberService(){
+        return new MemberService(memberRepository);
+    }
+
+    //@Componet사용 안할 시 의존성 주입
+    //@Bean
+    //public TimeTraceAop timeTraceAop(){return new TimeTraceAop();}
+
+}
